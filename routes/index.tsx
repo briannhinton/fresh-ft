8f40cf3019238e444c0ee42feec856ee16448012
Brian Hinton <briannhinton@gmail.com>
2022-07-03 21:56:07 -0400

    Feat: initial json db generation for cached data

--- routes/index.tsx
+++ routes/index.tsx
@@ -4,15 +4,31 @@ import { tw } from "@twind";
 import { Handlers, PageProps } from "$fresh/server.ts";
 import TimeSincePub from "../components/TimeSincePub.tsx";
 import LinkFooter from "../components/LinkFooter.tsx";
+import { Dbjson } from "dbjson";
+
+// this is a temporary hack to get the current date for a generated hash for a new db.json file
+const today = Date.now();
+const date = new Date(today);
+const day = date.getDate().toString();
+const month = (date.getMonth() + 1).toString();
+const year = date.getFullYear();
+const generatedDate = day + month + year;
 
 export const handler: Handlers<Project> = {
   async GET(__req, ctx) {
     const rawLinks = fetch("");
     const links = await (await rawLinks).json()
+    
+    // set the relative path of the json
+    const dbjson = new Dbjson(`./db/db-${generatedDate}.json`, `link`); 
 
+    // this gonna write it in the place and with name we choose it in the instance we did in "new Dbjson"
+    await dbjson.writeJSON(links);
+    // return links context for use with pageprops
     return ctx.render(links.records);
   }
 }
+
 // TODO: border right on main container
 export default function Home(props: PageProps) {
   const currentDate = Date.now()
